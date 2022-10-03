846ba771c7732bed908bbb97c69ca8b6231cb399
guillaumericard <ricard.g05@gmail.com>
2022-10-03 16:56:22 -0400

    changement login

--- gym-app/src/app/app.module.ts
+++ gym-app/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponentComponent } from './login-component/login-component.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule
